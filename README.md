1aa9787927b07d89e3264ebd8f56ab39269f693f
Bharath <reachbach@outlook.com>
2014-05-07 00:03:55 +0530

    Updated readme. Included general input errors.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-domaincrawler
+Domain Crawler
 =============
+
+A command line utility to crawl a single domain. When invoked from the console, it prints the site map as a digraph in the form of a sequence of 
+vertices and edges. 
+
+#Setup:
+To use the crawler:
+
+* Clone this repository: 
+* Create a virtualenv if need be
+* Change to the directory. Then change to the src directory.
+* Either in the global python environment or in the virtual env, run the following command : pip install -r requirements.txt
+* set the library path : export PYTHONPATH=$PWD:$PYTHONPATH
+* Run the following command to start crawling a domain : python domaincrawl/crawler_init.py --log_conf ../conf/logging.conf --url http://localhost:8000/
+* Alternatively, invoke the script with the -h option to see the following help message:
+
+>usage: crawler_init.py [-h] [--alt_conf_path ALT_CONF_PATH] --log_conf
+>                       LOG_CONF --url URL
+>
+>Crawl a single domain
+>
+>optional arguments:
+>  -h, --help            show this help message and exit
+>  --alt_conf_path ALT_CONF_PATH
+>                        Path to a python config (without sections) that may be
+>                        used to override config defaults
+>  --log_conf LOG_CONF   The location of the logging configuration file in the
+>                        python logging config format
+>  --url URL             The base url with the domain name of the site to be
+>                        crawled. (e.g. http://acme.com)
+
+Limitations and Enhancements:
+Refer to [this page](https://github.com/planBrk/domaincrawler/wiki/Current-limitations-&-Pending-enhancements) for details.
--- src/domaincrawl/crawler_init.py
+++ src/domaincrawl/crawler_init.py
@@ -1,5 +1,6 @@
 import argparse
 from threading import Event
+import traceback
 from domaincrawl.link_aggregator import LinkAggregator
 from domaincrawl.orchestrator import CrawlOrchestrator
 from domaincrawl import conf
@@ -13,48 +14,58 @@ from link_filters import DomainFilter, RobotsURLFilter, is_acceptable_url_scheme
 import logging
 
 def init_crawler():
-    parser = argparse.ArgumentParser(description='Start crawling a domain')
+    parser = argparse.ArgumentParser(description='Crawl a single domain')
     parser.add_argument('--alt_conf_path', required=False, help='Path to a python config (without sections) that may be used to override config defaults')
     parser.add_argument('--log_conf', required=True, help='The location of the logging configuration file in the python logging config format')
-    parser.add_argument('--url', required=True, help='The base url with the domain name (and not the IP) of the site to be crawled')
+    parser.add_argument('--url', required=True, help='The base url with the domain name of the site to be crawled. (e.g. http://acme.com)')
     input = vars(parser.parse_args())
     base_url = input['url']
-    base_domain, port = extract_domain_port(base_url)
-    conf_path = input.get('alt_conf_path', None)
-    configuration = None
-    if conf_path is None:
-        configuration = conf.get_default()
-    else:
-        configuration = conf.from_file(conf_path)
+    page_fetcher = None
+    logger = None
+    try:
+        base_domain, port = extract_domain_port(base_url)
+        conf_path = input.get('alt_conf_path', None)
+        configuration = None
+        if conf_path is None:
+            configuration = conf.get_default()
+        else:
+            configuration = conf.from_file(conf_path)
 
-    config.fileConfig(input['log_conf'])
-    logger = logging.getLogger(configuration.logger_name)
+        config.fileConfig(input['log_conf'])
+        logger = logging.getLogger(configuration.logger_name)
 
-    logger.debug("Base domain : %s"%base_domain)
+        logger.debug("Base domain : %s"%base_domain)
 
-    executor = ThreadPoolExecutor(max_workers=configuration.max_parser_workers)
-    termination_cond_var = Event()
-    url_norm = URLNormalizer(base_domain, port)
-    normalized_url = url_norm.normalize_with_domain(base_url)
-    logger.debug("Constructed normalized base url : %s"%normalized_url)
-    robots_fetch_timeout = configuration.connect_timeout + configuration.response_timeout
-    robots_filter = RobotsURLFilter(normalized_url, robots_fetch_timeout, configuration.user_agent, logger)
+        executor = ThreadPoolExecutor(max_workers=configuration.max_parser_workers)
+        termination_cond_var = Event()
+        url_norm = URLNormalizer(base_domain, port)
+        normalized_url = url_norm.normalize_with_domain(base_url)
+        logger.debug("Constructed normalized base url : %s"%normalized_url)
+        robots_fetch_timeout = configuration.connect_timeout + configuration.response_timeout
+        robots_filter = RobotsURLFilter(normalized_url, robots_fetch_timeout, configuration.user_agent, logger)
 
-    domain_filter = DomainFilter(base_domain, logger)
-    site_graph = SiteGraph(logger)
-    link_aggregator = LinkAggregator(logger, site_graph, link_mappers=[url_norm.normalize_with_domain], link_filters=[domain_filter.passes, robots_filter.passes,is_acceptable_url_scheme])
+        domain_filter = DomainFilter(base_domain, logger)
+        site_graph = SiteGraph(logger)
+        link_aggregator = LinkAggregator(logger, site_graph, link_mappers=[url_norm.normalize_with_domain], link_filters=[domain_filter.passes, robots_filter.passes,is_acceptable_url_scheme])
 
-    page_fetcher = PageFetcher(configuration, logger)
-    orchestrator = CrawlOrchestrator(executor, logger, termination_cond_var, extract_links, link_aggregator, page_fetcher.fetch_page)
-    page_fetcher.set_fetch_result_handler(orchestrator.handle_page_fetch_result)
+        page_fetcher = PageFetcher(configuration, logger)
+        orchestrator = CrawlOrchestrator(executor, logger, termination_cond_var, extract_links, link_aggregator, page_fetcher.fetch_page)
+        page_fetcher.set_fetch_result_handler(orchestrator.handle_page_fetch_result)
 
-    logger.info("Initiating crawl...")
-    orchestrator.init_crawl(normalized_url)
-    termination_cond_var.wait()
-    logger.info("Crawl complete. Shutting down...")
-    page_fetcher.shutdown()
-    logger.info("Site graph, in the form [array of pages] , [array of (src,dest) link tuples] follows: ")
-    logger.info(site_graph.stringize())
+        logger.info("Initiating crawl...")
+        orchestrator.init_crawl(normalized_url)
+        termination_cond_var.wait()
+        logger.info("Crawl complete. Shutting down...")
+    except:
+        print("Error initiating crawl.Ensure that a valid url is "
+                     "provided & that the destination site is reachable")
+        print traceback.print_exc()
+    if page_fetcher:
+        page_fetcher.shutdown()
+
+    if logger:
+        logger.info("Site graph, in the form [array of pages] , [array of (src,dest) link tuples] follows: ")
+        logger.info(site_graph.stringize())
 
 if __name__ == "__main__":
     init_crawler()
